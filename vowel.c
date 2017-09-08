1df9cd1752cf1094d57c9a94cc9af095d1ab627f
9080701209 <31505908+9080701209@users.noreply.github.com>
2017-09-08 06:52:51 -0700

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the charecter");
+scanf("%c",&ch);
+if(ch=='a';;ch=='e';;ch=='i';;ch=='o';;ch=='u';;ch=='A';;ch=='E';;ch=='I';;ch=='O';;ch=='U')
+{
+printf("entered num is vowel");
+}
+else
+{
+printf("the given num is consonant")
+}
+getch();
+}
