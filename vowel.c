806a61018b47110eaa2feffdc8b2b81edfe439fa
9080701209 <31505908+9080701209@users.noreply.github.com>
2017-09-08 06:56:13 -0700

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -7,11 +7,11 @@ printf("enter the charecter");
 scanf("%c",&ch);
 if(ch=='a';;ch=='e';;ch=='i';;ch=='o';;ch=='u';;ch=='A';;ch=='E';;ch=='I';;ch=='O';;ch=='U')
 {
-printf("entered num is vowel");
+printf("entered charecter is vowel");
 }
 else
 {
-printf("the given num is consonant")
+printf("the given charecter is consonant")
 }
 getch();
 }
