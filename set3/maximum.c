522043dddae930cba88ac57aab3e0818bf30f573
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-01-10 16:17:19 +0530

    Create maximum.c

---
+++ set3/maximum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int i,j,a[50].n,temp;
+printf("enter the array size");
+scanf("%d",&size);
+printf("enter the elements");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+printf("%d",a[i]);
+{
+for(i=o;i<n-1;i++)
+{
+for(j=1;j<n;j++)
+{
+if(a[i]<a[j])
+{
+scanf("%d",&n);
+}
+}
+}
+}
+}
