47eeb7c68a0c37e187da252235ab8703735e448f
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-01-21 10:33:30 +0530

    Update multiple.c

--- multiple.c
+++ multiple.c
@@ -4,9 +4,9 @@ main()
 int sum=0,i,n;
 printf("enter a value of n");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
+for(i=1;i<=5;i++)
 {
-printf("\n 5*%1d=%1d",i,5*i);
+printf("\n 5*%1d=%1d",i,n*i);
 }
 return 0;
 }
