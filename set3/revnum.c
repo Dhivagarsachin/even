ec48c56a840360484d27a5202d35285ecd4d6030
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-01-24 16:57:22 +0530

    Update revnum.c

--- set3/revnum.c
+++ set3/revnum.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int n, reversedNumber = 0, remainder;
+    int a, revNumber = 0, remainder;
 
     printf("Enter an integer: ");
-    scanf("%d", &n);
+    scanf("%d", &a);
 
     while(n != 0)
     {
-        remainder = n%10;
-        reversedNumber = reversedNumber*10 + remainder;
-        n /= 10;
+        remainder = a%10;
+        reversedNumber = revNumber*10 + remainder;
+        a /= 10;
     }
 
-    printf("Reversed Number = %d", reversedNumber);
+    printf("Reversed Number = %d", revNumber);
 
     return 0;
 }
